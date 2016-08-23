e0f81d4dca117786bf54543d351c996506d74d37
murilo0121 <murilo0121@gmail.com>
2016-08-23 01:13:22 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
+    //Só está funcionando por enquanto com letras minúsculas.
     char frase[] = "n ngvivqnqr qr ubwr fren pbafgehve hz cebtenzn an yvathntrz qr fhn cersrerapvn dhr rkrphgr n pbqvsvpnpnb r n qrpbqvsvpnpnb qr nedhvibf hfnaqb onfr. frh cebtenzn qrir fre pncnm qr yre r pbqvsvpne nedhvibf grkgb rz dhnydhre sbezngnpnb, orz pbzb nedhvibf ovanevbf. b erfhygnqb qrir fre hz nedhvib pbqvsvpnqb pbz b zrfzb abzr qb bevtvany.o vaqvpnaqb hz nedhvib onfr. cbfgne b erfhygnqb svany an fhn cnfgn crffbny ab rherxn";
     int shift = 0;
 
